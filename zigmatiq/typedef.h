c54321c7645a17385d4670109cb10c7306574547
Chase Zehl O'Byrne <zehl@live.com>
2023-12-22 13:44:40 -0500

    Add typedefs for integer types in zigmatiq/typedef.h

---
+++ zigmatiq/typedef.h
@@ -0,0 +1,39 @@
+/*
+ * ZIGMAtiq, Copyright (C) 2024 Chase Zehl O'Byrne
+ *   <mail: zehl@live.com> http://zehlchen.com/
+ *
+ * This file is part of ZIGMAtiq.
+ *
+ * ZIGMAtiq is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ZIGMAtiq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ZIGMAtiq; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#pragma once
+#ifndef _ZIGMATIQ_TYPEDEF_H_
+#define _ZIGMATIQ_TYPEDEF_H_
+
+#include <stdint.h>
+
+typedef uint8_t  uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t   int8;
+typedef int16_t  int16;
+typedef int32_t  int32;
+typedef int64_t  int64;
+
+#endif /* _ZIGMATIQ_TYPEDEF_H_ */
